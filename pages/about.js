b6be278f170090ad6d6876089d63f2ee33eda406
bengallagher <bgallag5@gmail.com>
2022-07-01 13:02:38 -0400

    wildlife images

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,23 @@ import PageHeader from "../components/UI/PageHeader";
 import imgSrc1 from "../assets/images/trail-pic-3.jpeg";
 import imgSrc2 from "../assets/images/trail-pic-9.jpg";
 
+import Image from "next/image";
+import wildlifeImg1 from "../assets/images/wildlife1.png";
+import wildlifeImg2 from "../assets/images/wildlife2.jpeg";
+import wildlifeImg3 from "../assets/images/wildlife5.jpeg";
+
 export default function about() {
+
+
+    // const arrangePics = () => {
+
+    // };
+
+    // const wildlifePics = document.querySelectorAll('.wildlife-img').forEach(pic => {
+
+    // })
+
+
   return (
     <div className="page-container flex-col">
       <PageHeader
@@ -13,7 +29,7 @@ export default function about() {
         subtitle="Find the Outdoors"
       />
       <div className="content test flex flex-col">
-        <div className=" w-full h-64">
+        <div className=" w-full h-32">
           <h2 className="content__title">Page Nav</h2>
           <nav>
             <ul className="flex flex-row w-full gap-5">
@@ -23,11 +39,28 @@ export default function about() {
             </ul>
           </nav>
         </div>
-        <div className="content test">
+        <div className="flex flex-row-reverse h-[40vh] w-full justify-center gap-10 items-center">
+          <div className="w-1/2 h-full flex flex-col items-start ">
             <h2 className="content__title">Wildlife</h2>
-            <div>
+            <p className="w-full ">
+              The Appalachian trail offers a variety of activities, but most
+              people come for the hiking. Both Day Hiking and Muli-Day Hiking,
+              also called Backpacking, are the two most popular uses for the
+              trail. Our Day Hike program is the perfect place to start for new
+              or inexperienced hikers. Guided Day hikes are between 3-6 hours
+              long with one or more of our guides. The minimun age for
+              participants is 10 years old, but all parents are urged to
+              consider their child's abilities, as the hikes can be challenging.
+            </p>
+          </div>
 
+          <div className="relative flex h-full w-1/2 object-cover">
+            <div className="relative h-full w-full">
+              <img src={wildlifeImg2} alt='wildlife' className="wildlife-img wildlife-img--1"></img>
+              <img src={wildlifeImg1} alt='wildlife' className="wildlife-img wildlife-img--2"></img>
+              <img src={wildlifeImg3} alt='wildlife' className="wildlife-img wildlife-img--3"></img>
             </div>
+          </div>
         </div>
       </div>
     </div>
@@ -37,3 +70,4 @@ export default function about() {
 //Nature
 //Gear
 //Trail Itself
+//Leave no tracce
